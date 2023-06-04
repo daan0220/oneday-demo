e25891c532a18e960f96ffe6e32ea3add86a2669
daikiando <daikiando@andoutaikinoMacBook-Pro.local>
2023-06-05 01:04:18 +0900

    既存ログイン機能とページの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './firebase';
 import Auth from './Auth';
-
-
+import { HomePage } from './HomePage';
 
 function App() {
-  return (
+  const [loggedIn, setLoggedIn] = useState<boolean>(false); // ログイン状態を管理する変数
 
-    <Router> 
-      <Auth />
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={loggedIn ? <Navigate to="/home" /> : <Auth setLoggedIn={setLoggedIn} />}
+        />
+        <Route
+          path="/home"
+          element={loggedIn ? <HomePage /> : <Navigate to="/" />}
+        />
+      </Routes>
     </Router>
-    
   );
 }
 
 export default App;
-
-
--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,96 +1,100 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { auth } from "./firebase";
-import {
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
-import { useNavigate } from "react-router-dom"
-import {
-  Box,
-  Button,
-  Container,
-  Grid,
-  TextField,
-} from "@mui/material";
-
-
-
-const Auth: React.FC = () => {
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, Container, Grid, TextField } from "@mui/material";
+type AuthProps = {
+    setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  };
+const Auth: React.FC<AuthProps> = ({ setLoggedIn }) => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const Register = async () => {
+  const register = async () => {
     await createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log(userCredential);
+        setLoggedIn(true); // ユーザーがログインしたらログイン状態をtrueに設定
         navigate("/home");
       })
       .catch((error) => {
         alert(error.message);
         console.error(error);
       });
+  };
 
-
-
-
+  const login = async () => {
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        setLoggedIn(true); // ユーザーがログインしたらログイン状態をtrueに設定
+        navigate("/home");
+      })
+      .catch((error) => {
+        alert(error.message);
+        console.error(error);
+      });
   };
 
   const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.currentTarget.value);
   };
+
   const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.currentTarget.value);
   };
 
   return (
-    <Fragment>
-      <Container>
-        <Grid container>
+    <Container>
+      <Grid container>
         <Grid item md={4}></Grid>
+        <Grid item md={4}>
           <Grid item md={4}>
-            <Grid item md={4}>
-              ログイン
-            </Grid>
-            <Box component="form">
-              <TextField
-                style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
-                name="email"
-                label="E-mail"
-                fullWidth
-                variant="outlined"
-                value={email}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  handleChangeEmail(event);
-                }}
-              />
-              <TextField
-                style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
-                name="password"
-                label="Password"
-                fullWidth
-                variant="outlined"
-                type="password"
-                value={password}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  handleChangePassword(event);
-                }}
-              />
-              <Button
-                fullWidth
-                style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
-                onClick={Register}
-              >
-                新規登録
-              </Button>
-            </Box>
+            ログイン
           </Grid>
-          <Grid item md={4}></Grid>
+          <Box component="form">
+            <TextField
+              style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+              name="email"
+              label="E-mail"
+              fullWidth
+              variant="outlined"
+              value={email}
+              onChange={handleChangeEmail}
+            />
+            <TextField
+              style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+              name="password"
+              label="Password"
+              fullWidth
+              variant="outlined"
+              type="password"
+              value={password}
+              onChange={handleChangePassword}
+            />
+            <Button
+              fullWidth
+              style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+              onClick={register}
+            >
+              新規登録
+            </Button>
+            <Button
+              fullWidth
+              style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+              onClick={login}
+            >
+              ログイン
+            </Button>
+          </Box>
         </Grid>
-      </Container>
-    </Fragment>
-    
+        <Grid item md={4}></Grid>
+      </Grid>
+    </Container>
   );
 };
 
 export default Auth;
+
