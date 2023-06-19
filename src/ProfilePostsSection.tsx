c6c43f4f59364c94821b094739ca46fa3ddf6b86
daikiando <daikiando@andoutaikinoMacBook-Pro.local>
2023-06-19 16:55:33 +0900

    投稿内容編集機能追加

--- src/ProfilePostsSection.tsx
+++ src/ProfilePostsSection.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
-import { Typography, Box, Paper, IconButton } from '@mui/material';
-import { MoreHoriz as MoreHorizIcon, Favorite as FavoriteIcon, FavoriteBorder as FavoriteBorderIcon } from '@mui/icons-material';
+import React, { useState } from 'react';
+import {
+  Typography,
+  Box,
+  Paper,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Button,
+} from '@mui/material';
+import {
+  MoreHoriz as MoreHorizIcon,
+  Favorite as FavoriteIcon,
+  FavoriteBorder as FavoriteBorderIcon,
+} from '@mui/icons-material';
 
 interface Post {
   studyTheme: string;
@@ -11,7 +26,7 @@ interface Post {
 
 interface ProfilePostsSectionProps {
   posts: Post[];
-  handleEditPost: (index: number) => void;
+  handleEditPost: (index: number, updatedPost: string) => void;
   handleLikePost: (index: number) => void;
 }
 
@@ -20,6 +35,30 @@ const ProfilePostsSection: React.FC<ProfilePostsSectionProps> = ({
   handleEditPost,
   handleLikePost,
 }) => {
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+  const [editPost, setEditPost] = useState<string>('');
+
+  const handleOpenEditDialog = (index: number, initialPost: string) => {
+    setEditIndex(index);
+    setEditPost(initialPost);
+  };
+
+  const handleCloseEditDialog = () => {
+    setEditIndex(null);
+    setEditPost('');
+  };
+
+  const handleEditPostChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditPost(event.target.value);
+  };
+
+  const handleSaveEditPost = () => {
+    if (editIndex !== null && editPost !== '') {
+      handleEditPost(editIndex, editPost);
+      handleCloseEditDialog();
+    }
+  };
+
   return (
     <Box sx={{ marginTop: '20px' }}>
       {posts.length > 0 ? (
@@ -40,7 +79,7 @@ const ProfilePostsSection: React.FC<ProfilePostsSectionProps> = ({
                 mt: 2,
               }}
             >
-              <IconButton onClick={() => handleEditPost(index)}>
+              <IconButton onClick={() => handleOpenEditDialog(index, post.additionalText)}>
                 <MoreHorizIcon />
               </IconButton>
               <Box sx={{ display: 'flex', alignItems: 'center' }}>
@@ -57,6 +96,24 @@ const ProfilePostsSection: React.FC<ProfilePostsSectionProps> = ({
       ) : (
         <Typography variant="body1">No posts yet.</Typography>
       )}
+
+      <Dialog open={editIndex !== null} onClose={handleCloseEditDialog}>
+        <DialogTitle>Edit Post</DialogTitle>
+        <DialogContent>
+          <TextField
+            multiline
+            rows={4}
+            variant="outlined"
+            value={editPost}
+            onChange={handleEditPostChange}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseEditDialog}>Cancel</Button>
+          <Button onClick={handleSaveEditPost}>Save</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
